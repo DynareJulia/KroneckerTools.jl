76feed1df1dec3661d261ad65ae677fd634bc5ce
MichelJuillard <michel.juillard@mjui.fr>
2020-11-30 09:02:49 +0100

    adding tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ import KroneckerTools: kron_mul_elem_t!, a_mul_kron_b!,
     a_mul_b_kron_c!, a_mul_b_kron_ct!, at_mul_b_kron_c!,
     a_mul_b_kron_c_d!,
     kron_a_mul_b!, kron_at_mul_b!,
-    kron_at_kron_b_mul_c!
+    kron_at_kron_b_mul_c!, QUT.QuasiUpperTriangular
 
 Random.seed!(123)
 #a = rand(2,3)
@@ -152,6 +152,102 @@ println("test4")
 nb = mc^order
 b = rand(ma,nb)
 d = rand(na,nc^order)
+work1 = rand(na*max(mc, nc)^order)
+work2 = similar(work1)
+at_mul_b_kron_c!(d, a, b, c, order, work1, work2)
+@time at_mul_b_kron_c!(d, a, b, c, order, work1, work2)
+@test d ≈ a'*b*kron(c,c)
+
+println("test1a")
+order = 2
+ma = 4
+na = 4
+q = 2
+a = QuasiUpperTriangular(triu(rand(ma, na)))
+b = rand(q*na^order)
+c = rand(q*ma^order)
+work1 = rand(q*max(ma, na)^order)
+work2 = similar(work1)
+kron_a_mul_b!(c, a, order, b, q, work1, work2)
+@time kron_a_mul_b!(c, a, order, b, q, work1, work2)
+@test c ≈ kron(kron(a,a),Matrix{Float64}(I(q)))*b
+
+println("test2a")
+b = rand(q*ma^order)
+c = rand(q*na^order)
+kron_at_mul_b!(c, a, order, b, q, work1, work2)
+@time kron_at_mul_b!(c, a, order, b, q, work1, work2)
+@test c ≈ kron(kron(a',a'),Matrix{Float64}(I(q)))*b
+
+println("test3a")
+order = 2
+mc = 2
+nc = 3
+c = rand(mc,nc)
+ma = 4
+na = 4
+a = rand(ma,na)
+nb = nc^order
+b = rand(na,nb)
+d = rand(ma,mc^order)
+work1 = rand(ma*max(mc, nc)^order)
+work2 = similar(work1)
+a_mul_b_kron_ct!(d, a, b, c, order, work1, work2)
+@time a_mul_b_kron_ct!(d, a, b, c, order, work1, work2)
+@test d ≈ a*b*kron(c',c')
+
+println("test4a")
+nb = mc^order
+b = rand(ma,nb)
+d = rand(na,nc^order)
+work1 = rand(na*max(mc, nc)^order)
+work2 = similar(work1)
+at_mul_b_kron_c!(d, a, b, c, order, work1, work2)
+@time at_mul_b_kron_c!(d, a, b, c, order, work1, work2)
+@test d ≈ a'*b*kron(c,c)
+
+println("test1b")
+order = 2
+ma = 4
+na = 4
+q = 2
+a = QuasiUpperTriangular(triu(rand(ma, na)))
+b = view(rand(q*na^order),:)
+c = view(rand(q*ma^order),:)
+work1 = rand(q*max(ma, na)^order)
+work2 = similar(work1)
+kron_a_mul_b!(c, a, order, b, q, work1, work2)
+@time kron_a_mul_b!(c, a, order, b, q, work1, work2)
+@test c ≈ kron(kron(a,a),Matrix{Float64}(I(q)))*b
+
+println("test2b")
+b = view(rand(q*ma^order),:)
+c = view(rand(q*na^order),:)
+kron_at_mul_b!(c, a, order, b, q, work1, work2)
+@time kron_at_mul_b!(c, a, order, b, q, work1, work2)
+@test c ≈ kron(kron(a',a'),Matrix{Float64}(I(q)))*b
+
+println("test3b")
+order = 2
+mc = 2
+nc = 3
+c = view(rand(mc,nc),:)
+ma = 4
+na = 4
+a = rand(ma,na)
+nb = nc^order
+b = view(rand(na,nb),:)
+d = view(rand(ma,mc^order),:)
+work1 = rand(ma*max(mc, nc)^order)
+work2 = similar(work1)
+a_mul_b_kron_ct!(d, a, b, c, order, work1, work2)
+@time a_mul_b_kron_ct!(d, a, b, c, order, work1, work2)
+@test d ≈ a*b*kron(c',c')
+
+println("test4b")
+nb = mc^order
+b = view(rand(ma,nb),:)
+d = view(rand(na,nc^order),:)
 work1 = rand(na*max(mc, nc)^order)
 work2 = similar(work1)
 at_mul_b_kron_c!(d, a, b, c, order, work1, work2)
